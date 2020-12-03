84a2ff2c0267ee54df83fa95f4a2006e40c94138
DAVID-OA <david.oconte@orange.fr>
2020-12-03 11:08:57 +0100

    I add the first resources from the training application.

--- app/src/main/java/com/oconte/david/go4lunch/HomeFragment.java
+++ app/src/main/java/com/oconte/david/go4lunch/HomeFragment.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 public class HomeFragment extends Fragment {
 
     public static HomeFragment newInstance() {
--- app/src/main/java/com/oconte/david/go4lunch/SettingsActivity.java
+++ app/src/main/java/com/oconte/david/go4lunch/SettingsActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.toolbar)androidx.appcompat.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
