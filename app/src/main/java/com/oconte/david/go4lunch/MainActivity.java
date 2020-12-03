017cffa69dc8e3e169193fff07128ef9cd167839
DAVID-OA <david.oconte@orange.fr>
2020-12-03 13:56:34 +0100

    I add the first resources from the training application.

--- app/src/main/java/com/oconte/david/go4lunch/MainActivity.java
+++ app/src/main/java/com/oconte/david/go4lunch/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
