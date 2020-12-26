6e7b27e57bdcb8910bff0c44e15f887680a21f3a
DAVID-OA <david.oconte@orange.fr>
2020-12-26 16:56:42 +0100

    I clean the code and add toolbar.xml for include the toolbar in the differents layout.

--- app/src/main/java/com/oconte/david/go4lunch/SettingsActivity.java
+++ app/src/main/java/com/oconte/david/go4lunch/SettingsActivity.java
@@ -3,13 +3,14 @@ package com.oconte.david.go4lunch;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar)androidx.appcompat.widget.Toolbar toolbar;
+    @BindView(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
