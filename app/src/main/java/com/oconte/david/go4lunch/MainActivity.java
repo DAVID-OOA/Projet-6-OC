2a965c60e4f66ba12dbf6f76b027ff46b64850ba
DAVID-OA <david.oconte@orange.fr>
2021-01-03 16:22:42 +0100

    I add the Binding far all Id but not for Toolbar.

--- app/src/main/java/com/oconte/david/go4lunch/MainActivity.java
+++ app/src/main/java/com/oconte/david/go4lunch/MainActivity.java
@@ -11,12 +11,14 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 import com.oconte.david.go4lunch.auth.AuthActivity;
+import com.oconte.david.go4lunch.databinding.ActivityMainBinding;
 import com.oconte.david.go4lunch.workMates.FragmentWorkMates;
 
 import butterknife.BindView;
@@ -25,10 +27,12 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @BindView(R.id.toolbar) Toolbar toolbar;
-    @BindView(R.id.bottom_nav) BottomNavigationView bottomNavigationView;
-    @BindView(R.id.activity_main_drawerLayout) DrawerLayout drawerLayout;
-    @BindView(R.id.activity_main_nav_view) NavigationView navigationView;
+    //@BindView(R.id.bottom_nav) BottomNavigationView bottomNavigationView;
+    //@BindView(R.id.activity_main_drawerLayout) DrawerLayout drawerLayout;
+    //@BindView(R.id.activity_main_nav_view) NavigationView navigationView;
 
+
+    private ActivityMainBinding binding;
 
     // 1 - Identifier for Sign-In Activity
     private static final int RC_SIGN_IN = 123;
@@ -56,7 +60,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
         ButterKnife.bind(this);
 
         this.startAuthActivity();
@@ -94,8 +100,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     public void onBackPressed() {
         // 5 - Handle back click to close menu
-        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            this.drawerLayout.closeDrawer(GravityCompat.START);
+        if (binding.activityMainDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            binding.activityMainDrawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
@@ -106,8 +112,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
      */
     private void configureDrawerLayout(){
         //this.drawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawerLayout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, binding.activityMainDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        binding.activityMainDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
     }
 
@@ -116,7 +122,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
      */
     private void configureNavigationView(){
         //this.navigationView = (NavigationView) findViewById(R.id.activity_main_nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        binding.activityMainNavView.setNavigationItemSelectedListener(this);
     }
     @SuppressLint("NonConstantResourceId")
     @Override
@@ -139,7 +145,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
         }
 
-        this.drawerLayout.closeDrawer(GravityCompat.START);
+        binding.activityMainDrawerLayout.closeDrawer(GravityCompat.START);
 
         return true;
     }
@@ -206,7 +212,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     private void configureBottomView() {
-        bottomNavigationView.setOnNavigationItemSelectedListener(item-> onNavigationItemSelected(item.getItemId()));
+        binding.bottomNav.setOnNavigationItemSelectedListener(item-> onNavigationItemSelected(item.getItemId()));
     }
 
     // ---------------------
--- app/src/main/java/com/oconte/david/go4lunch/SettingsActivity.java
+++ app/src/main/java/com/oconte/david/go4lunch/SettingsActivity.java
@@ -1,21 +1,27 @@
 package com.oconte.david.go4lunch;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import butterknife.BindView;
+import com.oconte.david.go4lunch.databinding.ActivitySettingsBinding;;import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    //For Design
+    private ActivitySettingsBinding binding;
     @BindView(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
+        binding = ActivitySettingsBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
         ButterKnife.bind(this);
 
         this.configureToolbar();
