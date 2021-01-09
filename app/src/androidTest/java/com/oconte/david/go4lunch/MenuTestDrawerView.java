0c348da8f913b579fd7c332434a18938fa7fe3d3
DAVID-OA <david.oconte@orange.fr>
2021-01-09 11:42:20 +0100

    I add the include toolbar.

--- app/src/androidTest/java/com/oconte/david/go4lunch/MenuTestDrawerView.java
+++ app/src/androidTest/java/com/oconte/david/go4lunch/MenuTestDrawerView.java
@@ -1,6 +1,10 @@
 package com.oconte.david.go4lunch;
 
+import android.view.Gravity;
+
+import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
@@ -11,10 +15,14 @@ import java.io.IOException;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 @RunWith(AndroidJUnit4.class)
 public class MenuTestDrawerView {
+
+
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(MainActivity.class, false, false);
@@ -22,14 +30,39 @@ public class MenuTestDrawerView {
     @Test
     public void testClickActionBarItemMenuDrawer() throws IOException, InterruptedException {
 
+        // a mettre en variable .InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        //j'ouvre ma sharedpreference et je lui met la valeur true.
+        //ajouter un idling ressources peut etre.
+        //Start the MainActivity
+        mActivityRule.launchActivity(null);
+        //resultLog =
+
+
+        //Select the toolbar
+        onView(withId(R.id.toolbar)).perform(click());
+
+        // Select the drawer menu
+        onView(withId(R.id.activity_main_nav_view))
+                .check(matches(isClosed(Gravity.LEFT)))
+                .perform(DrawerActions.open());
+
+        //Select the first item
+        //onView(withId(R.id.activity_main_drawer_lunch)).perform(click());
+
+
+
+    }
+
+    @Test
+    public void testClickActionBar() throws IOException, InterruptedException {
+
         //Start the MainActivity
         mActivityRule.launchActivity(null);
 
         //Select the toolbar
         onView(withId(R.id.toolbar)).perform(click());
 
-        //Select the first item
-        //onView(withId(R.id.activity_main_drawer_lunch)).perform(click());
 
 
 
