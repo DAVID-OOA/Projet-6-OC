cefebeac23d4c0c2b4e6f6de3ded3949f5df9d67
DAVID-OA <david.oconte@orange.fr>
2021-02-10 16:32:45 +0100

    I add test for Menu Drawer and for Bottom Menu.

--- app/src/androidTest/java/com/oconte/david/go4lunch/MenuTestDrawerView.java
+++ app/src/androidTest/java/com/oconte/david/go4lunch/MenuTestDrawerView.java
@@ -10,6 +10,10 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenu;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.oconte.david.go4lunch.auth.AuthActivity;
 
 import org.junit.Rule;
@@ -38,7 +42,7 @@ public class MenuTestDrawerView {
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<MainActivity>(MainActivity.class, false, false);
 
     @Test
-    public void testClickActionBarItemMenuDrawer() throws IOException, InterruptedException {
+    public void testClickActionBarItemMenuDrawerLunch() throws IOException, InterruptedException {
 
         SharedPreferences preferences = InstrumentationRegistry.getInstrumentation().getTargetContext().getSharedPreferences("EXTRA_LOG", MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
@@ -58,12 +62,95 @@ public class MenuTestDrawerView {
                 .perform(DrawerActions.open());
 
 
-        // Select the drawer menu
+        // Select the drawer menu for lunch button
         onView(withId(R.id.activity_main_nav_view))
                 .perform(NavigationViewActions.navigateTo(R.id.activity_main_drawer_lunch));
+        Thread.sleep(1000);
 
-        //Select the first item
-        //onView(withId(R.id.activity_main_drawer_lunch)).perform(click());
+    }
+
+    @Test
+    public void testClickActionBarItemMenuDrawerSettings() throws IOException, InterruptedException {
+
+        SharedPreferences preferences = InstrumentationRegistry.getInstrumentation().getTargetContext().getSharedPreferences("EXTRA_LOG", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(EXTRA_IS_CONNECTED, true);
+        editor.apply();
+
+        //Start the MainActivity
+        mActivityRule.launchActivity(null);
+
+        //Select the toolbar
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
+
+
+        // Open Drawer to click on navigation.
+        onView(withId(R.id.activity_main_drawerLayout))
+                .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
+                .perform(DrawerActions.open());
+
+        // Select the drawer menu for settings button
+        onView(withId(R.id.activity_main_nav_view))
+                .perform(NavigationViewActions.navigateTo(R.id.activity_main_drawer_settings));
+        Thread.sleep(1000);
+
+    }
+
+    @Test
+    public void testClickActionBarItemMenuDrawerLogOut() throws IOException, InterruptedException {
+
+        SharedPreferences preferences = InstrumentationRegistry.getInstrumentation().getTargetContext().getSharedPreferences("EXTRA_LOG", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(EXTRA_IS_CONNECTED, true);
+        editor.apply();
+
+        //Start the MainActivity
+        mActivityRule.launchActivity(null);
+
+        //Select the toolbar
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
+
+
+        // Open Drawer to click on navigation.
+        onView(withId(R.id.activity_main_drawerLayout))
+                .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
+                .perform(DrawerActions.open());
+
+        // Select the drawer menu for logout button
+        onView(withId(R.id.activity_main_nav_view))
+                .perform(NavigationViewActions.navigateTo(R.id.activity_main_drawer_logout));
+
+    }
+
+    @Test
+    public void testClickBottomNavigationView() throws IOException, InterruptedException {
+
+        SharedPreferences preferences = InstrumentationRegistry.getInstrumentation().getTargetContext().getSharedPreferences("EXTRA_LOG", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(EXTRA_IS_CONNECTED, true);
+        editor.apply();
+
+        //Start the MainActivity
+        mActivityRule.launchActivity(null);
+
+        // Select the bottom view menu for map button
+        onView(withId(R.id.bottom_nav))
+                .check(matches(isDisplayed()));
+        Thread.sleep(1000);
+
+
+        onView(withId(R.id.action_map))
+                .check(matches(isDisplayed())).perform(click());
+        Thread.sleep(1000);
+
+        onView(withId(R.id.action_list))
+                .check(matches(isDisplayed())).perform(click());
+        Thread.sleep(1000);
+
+        onView(withId(R.id.action_workmates))
+                .check(matches(isDisplayed())).perform(click());
+                //.perform(BottomNavigationMenuView.navigateTo(R.id.action_map));
+
 
 
     }
