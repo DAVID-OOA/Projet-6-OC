0e737d6bad0f0c94e3a04b077e11688f951105b6
DAVID-OA <david.oconte@orange.fr>
2021-02-18 16:14:28 +0100

    I add googlemap.

--- app/src/main/java/com/oconte/david/go4lunch/mapView/FragmentMapView.java
+++ app/src/main/java/com/oconte/david/go4lunch/mapView/FragmentMapView.java
@@ -8,13 +8,23 @@ import android.view.ViewGroup;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.oconte.david.go4lunch.R;
+import com.oconte.david.go4lunch.databinding.FragmentMapViewBinding;
 
 import java.util.Objects;
 
 import butterknife.ButterKnife;
 
-public class FragmentMapView extends Fragment {
+public class FragmentMapView extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
+    private MapView mapView;
+
+    private FragmentMapViewBinding binding;
 
     public static FragmentMapView newInstance() {
         return (new FragmentMapView());
@@ -22,11 +32,50 @@ public class FragmentMapView extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_map_view, container, false);
-        ButterKnife.bind(this, view);
+        binding = FragmentMapViewBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
 
         Objects.requireNonNull(((AppCompatActivity) Objects.requireNonNull(getActivity())).getSupportActionBar()).setTitle("I'm Hungry !");
 
+        this.configureMapView(savedInstanceState);
+
         return view;
+    }
+
+    private void configureMapView(Bundle savedInstanceState) {
+        mapView = (MapView) binding.mapView;
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
