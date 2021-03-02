5dd1d75aeb206bcd81c2a93f79664fca163a011e
DAVID-OA <david.oconte@orange.fr>
2021-03-02 14:21:04 +0100

    I add for API.

--- app/src/main/java/com/oconte/david/go4lunch/Injection.java
+++ app/src/main/java/com/oconte/david/go4lunch/Injection.java
@@ -2,6 +2,10 @@ package com.oconte.david.go4lunch;
 
 import androidx.test.espresso.idling.CountingIdlingResource;
 
+import com.oconte.david.go4lunch.api.GooglePlaceFactory;
+import com.oconte.david.go4lunch.api.GooglePlaceService;
+import com.oconte.david.go4lunch.listView.GooglePlaceCallRestaurantNearBy;
+
 public class Injection {
 
     public static final CountingIdlingResource resource = new CountingIdlingResource("GooglePlaceIdling");
@@ -15,6 +19,12 @@ public class Injection {
     public static GooglePlaceCallRestaurantDetails getRestaurantDetail (GooglePlaceService googlePlaceService, CountingIdlingResource resource) {
         GooglePlaceCallRestaurantDetails restaurantDetails = new GooglePlaceCallRestaurantDetails(googlePlaceService, resource);
         return restaurantDetails;
+    }
+
+    //For Call getRestaurantDetail
+    public static GooglePlaceCallRestaurantNearBy getRestaurantNearBy (GooglePlaceService googlePlaceService, CountingIdlingResource resource) {
+        GooglePlaceCallRestaurantNearBy restaurantNearBy = new GooglePlaceCallRestaurantNearBy(googlePlaceService, resource);
+        return restaurantNearBy;
     }
 
 
---
+++ app/src/main/java/com/oconte/david/go4lunch/api/GooglePlaceService.java
@@ -0,0 +1,23 @@
+package com.oconte.david.go4lunch.api;
+
+import com.oconte.david.go4lunch.models.ApiNearByResponse;
+import com.oconte.david.go4lunch.models.ApiRestaurantDetails;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface GooglePlaceService {
+
+    public static String API_KEY = "AIzaSyBq5YQ-3LwqD0gaRZ9B7zJf5tYw2qhSQHo";
+
+
+
+    @GET("place/details/json?fields=vicinity,place_id,id,geometry,opening_hours,name,rating,photo&key=" + API_KEY)
+    Call<ApiRestaurantDetails> getRestaurantDetail(@Query("placeid") String placeId);
+
+    @GET("place/nearbysearch/json?location=44.532616, 0.768252&radius=3000&key=" + API_KEY)
+    Call<ApiNearByResponse> getRestaurantNearBy(@Query("location") String location);
+
+
+}
