f3f33e4d2461e7eb962be364bea2f611438fb333
DAVID-OA <david.oconte@orange.fr>
2021-03-05 14:34:08 +0100

    I add the star for rating.

---
+++ app/src/main/java/com/oconte/david/go4lunch/util/ForRating.java
@@ -0,0 +1,25 @@
+package com.oconte.david.go4lunch.util;
+
+import com.oconte.david.go4lunch.R;
+
+public class ForRating {
+
+    public static int calculateRating(Double rating) {
+        if (rating == 0) return 0;
+        if (rating < 0.84) return 1;
+        if (rating < 1.68) return 2;
+        if (rating < 2.52) return 3;
+        if (rating < 3.36) return 4;
+        if (rating < 4.7) return 5;
+        return 6;
+    }
+
+    public static int firstStar (int rating) {
+        if (rating <= 0) return R.drawable.star_border;
+        if (rating ==1) return R.drawable.star_half;
+        return R.drawable.star;
+    }
+
+
+
+}
