18c3c8d8e3aa00e01e6a45159af2dcf9946fa76c
DAVID-OA <david.oconte@orange.fr>
2021-07-03 17:08:55 +0200

    I add the onClickRecyclerVIew.

--- app/src/main/java/com/oconte/david/go4lunch/listView/FragmentListViewRestaurant.java
+++ app/src/main/java/com/oconte/david/go4lunch/listView/FragmentListViewRestaurant.java
@@ -1,5 +1,6 @@
 package com.oconte.david.go4lunch.listView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +11,13 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.oconte.david.go4lunch.R;
 import com.oconte.david.go4lunch.databinding.FragmentListViewBinding;
 import com.oconte.david.go4lunch.models.Result;
+import com.oconte.david.go4lunch.restoDetails.DetailsRestaurantActivity;
+import com.oconte.david.go4lunch.util.ItemClickSupport;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -31,14 +36,16 @@ public class FragmentListViewRestaurant extends Fragment {
 
     @Override
     public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-         binding = FragmentListViewBinding.inflate(inflater, container, false);
-         View view = binding.getRoot();
+        binding = FragmentListViewBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
 
         Objects.requireNonNull(((AppCompatActivity) requireActivity()).getSupportActionBar()).setTitle("I'm Hungry !");
 
         this.configureViewModel();
 
         this.configureRecyclerView();
+
+        this.configureOnClickRecyclerView();
 
         return view;
     }
@@ -68,6 +75,18 @@ public class FragmentListViewRestaurant extends Fragment {
 
         // Set layout manager to position the items
         this.binding.fragmentMainRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    private void configureOnClickRecyclerView(){
+        ItemClickSupport.addTo(binding.fragmentMainRecyclerView, R.layout.detail_view_resto)
+                .setOnItemClickListener((recyclerView, position, v) -> {
+                    
+                    //result.articles.get(position);
+
+                    Intent intent = new Intent(getContext(), DetailsRestaurantActivity.class);
+                    startActivity(intent);
+                });
+
     }
 
 }
