7d4c4666400061f3f5a61028ac7624a5bd3850e3
DAVID-OA <david.oconte@orange.fr>
2021-07-19 19:10:11 +0200

    I clean the code.

--- app/src/main/java/com/oconte/david/go4lunch/workMates/FragmentWorkMates.java
+++ app/src/main/java/com/oconte/david/go4lunch/workMates/FragmentWorkMates.java
@@ -5,9 +5,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.oconte.david.go4lunch.databinding.FragmentWorkmatesBinding;
 
 import org.jetbrains.annotations.NotNull;
@@ -16,6 +22,7 @@ import java.util.Objects;
 
 public class FragmentWorkMates extends Fragment {
 
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     public static FragmentWorkMates newInstance() {
         return (new FragmentWorkMates());
@@ -32,5 +39,17 @@ public class FragmentWorkMates extends Fragment {
         return view;
     }
 
+
+
+
+    public void getDocument() {
+        DocumentReference docRef = db.collection("workMates").document("USER");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<DocumentSnapshot> task) {
+
+            }
+        });
+    }
 
 }
