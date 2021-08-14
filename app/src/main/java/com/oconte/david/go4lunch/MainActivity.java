1efaff975e3904006b78ffe67e8210e399cd09af
DAVID-OA <david.oconte@orange.fr>
2021-08-14 11:48:13 +0200

    I clean the code.

--- app/src/main/java/com/oconte/david/go4lunch/MainActivity.java
+++ app/src/main/java/com/oconte/david/go4lunch/MainActivity.java
@@ -1,6 +1,7 @@
 package com.oconte.david.go4lunch;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -13,6 +14,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.fragment.app.Fragment;
@@ -122,13 +124,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         startActivity(intent);
     }
 
-
     //For SIGN OUT
 
     // It's for sign out and restart AuthActivity
     private void resultSignOut() {
         this.signOutUserFromFirebase();
-
     }
 
     // When you log out save the state for the next launch application.
@@ -160,7 +160,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error deleting document", e);
+                        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                        alertDialog.setTitle("Error");
+                        alertDialog.setMessage("Your auth is not worked, try again");
+                        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                        alertDialog.show();
                     }
                 });
     }
@@ -220,7 +229,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-
         // Handle Navigation Item Click
         int id = item.getItemId();
 
@@ -274,8 +282,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         binding.bottomNav.setOnNavigationItemSelectedListener(item -> onNavigationItemSelected(item.getItemId()));
 
     }
-
-
 
 
     // ---------------------
---
+++ app/src/main/java/com/oconte/david/go4lunch/util/ForNetIsAvailable.java
@@ -0,0 +1,25 @@
+package com.oconte.david.go4lunch.util;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class ForNetIsAvailable {
+
+
+    private static boolean netIsAvailable() {
+        try {
+            final URL url = new URL("http://www.google.com");
+            final URLConnection conn = url.openConnection();
+            conn.connect();
+            conn.getInputStream().close();
+            return true;
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+}
