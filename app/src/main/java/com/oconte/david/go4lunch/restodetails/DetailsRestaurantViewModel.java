105a279fbead5a200c86c571d0ba816d6a7e82db
DAVID-OOA <daoconteid@gmail.com>
2021-11-06 12:15:42 +0100

    I clean the code.

--- app/src/main/java/com/oconte/david/go4lunch/restodetails/DetailsRestaurantViewModel.java
+++ app/src/main/java/com/oconte/david/go4lunch/restodetails/DetailsRestaurantViewModel.java
@@ -10,7 +10,6 @@ import androidx.lifecycle.ViewModel;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
