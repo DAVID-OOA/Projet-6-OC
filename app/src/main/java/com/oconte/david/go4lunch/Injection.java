449840b17d8cbf93f4926ea5ebcc0850ba327172
DAVID-OOA <daoconteid@gmail.com>
2021-11-06 12:16:36 +0100

    I clean the code and add ViewModelDetailsRestaurantFactory().

--- app/src/main/java/com/oconte/david/go4lunch/Injection.java
+++ app/src/main/java/com/oconte/david/go4lunch/Injection.java
@@ -2,10 +2,13 @@ package com.oconte.david.go4lunch;
 
 import androidx.test.espresso.idling.CountingIdlingResource;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.oconte.david.go4lunch.api.GooglePlaceFactory;
 import com.oconte.david.go4lunch.api.GooglePlaceService;
 import com.oconte.david.go4lunch.listView.RestaurantRepository;
 import com.oconte.david.go4lunch.restodetails.RestaurantDetailRepository;
+import com.oconte.david.go4lunch.restodetails.ViewModelDetailsRestaurantFactory;
 import com.oconte.david.go4lunch.workMates.UserRepository;
 
 public class Injection {
@@ -15,12 +18,6 @@ public class Injection {
     public static GooglePlaceService getService() {
         return GooglePlaceFactory.getRetrofit().create(GooglePlaceService.class);
     }
-
-    /*For Call getRestaurantDetail
-    public static RestaurantDetailRepository getRestaurantDetail (GooglePlaceService googlePlaceService, CountingIdlingResource resource) {
-        RestaurantDetailRepository restaurantDetails = new RestaurantDetailRepository(googlePlaceService, resource);
-        return restaurantDetails;
-    }*/
 
     //For Call getRestaurantNearBy
     public static RestaurantRepository getRestaurantNearBy (GooglePlaceService googlePlaceService, CountingIdlingResource resource) {
@@ -34,10 +31,15 @@ public class Injection {
         return userRepository;
     }
 
-    //For Call getRestaurantDetailsRepository
-    public static RestaurantDetailRepository getRestaurantDetailsRepository() {
-        RestaurantDetailRepository restaurantDetailRepository = new RestaurantDetailRepository();
-        return restaurantDetailRepository;
+    public static RestaurantDetailRepository provideRestaurantDetailsRepository(FirebaseAuth firebaseAuth, FirebaseFirestore firebaseFirestore) {
+        return new RestaurantDetailRepository(firebaseAuth, firebaseFirestore);
+    }
+
+    //For Call ViewModelFactory
+    public static ViewModelDetailsRestaurantFactory provideViewModelFactory(FirebaseAuth firebaseAuth, FirebaseFirestore firebaseFirestore){
+        RestaurantDetailRepository restaurantDetailRepository = provideRestaurantDetailsRepository(firebaseAuth,firebaseFirestore);
+
+        return  new ViewModelDetailsRestaurantFactory(restaurantDetailRepository, getUserRepository());
     }
 
 }
