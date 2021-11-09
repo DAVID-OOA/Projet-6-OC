c913012d8e6999f6229891cf4a00fbdb037e8f0a
DAVID-OOA <daoconteid@gmail.com>
2021-11-09 15:14:55 +0100

    I add the Adapter for the list of User picked the restaurant for eat.

---
+++ app/src/main/java/com/oconte/david/go4lunch/restodetails/DetailsRestaurantPickedByUserAdapter.java
@@ -0,0 +1,41 @@
+package com.oconte.david.go4lunch.restodetails;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.oconte.david.go4lunch.databinding.RestoUserItemRecyclerViewBinding;
+import com.oconte.david.go4lunch.models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetailsRestaurantPickedByUserAdapter extends RecyclerView.Adapter<DetailsRestaurantPickedByUserViewHolder> {
+
+    private List<User> usersPicked = new ArrayList<>();
+
+
+    @NonNull
+    @Override
+    public DetailsRestaurantPickedByUserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new DetailsRestaurantPickedByUserViewHolder(RestoUserItemRecyclerViewBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DetailsRestaurantPickedByUserViewHolder viewHolder, int position) {
+        viewHolder.updateWithUserPicked(usersPicked.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.usersPicked.size();
+    }
+
+    public void updateCallUserListPicked(List<User> userListPicked) {
+        this.usersPicked = userListPicked;
+        this.notifyDataSetChanged();
+    }
+}
