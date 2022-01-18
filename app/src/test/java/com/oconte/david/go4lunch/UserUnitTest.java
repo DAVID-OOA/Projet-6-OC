c20a986e90be41a3d2b46520664fa5ad5da1d8c2
Oconte David <david.oconte@orange.fr>
2022-01-18 11:23:54 +0100

    I add for test User.

---
+++ app/src/test/java/com/oconte/david/go4lunch/UserUnitTest.java
@@ -0,0 +1,35 @@
+package com.oconte.david.go4lunch;
+
+import static org.junit.Assert.assertEquals;
+
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+
+import com.oconte.david.go4lunch.models.User;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4ClassRunner.class)
+public class UserUnitTest {
+    private User user;
+    private String uid;
+    private String username;
+    private String email;
+    private String urlPicture;
+    private String idRestaurantPicked;
+
+    @Before
+    public void setup() {
+        username = "Android Studio";
+        uid = "01012022";
+        email = "androidstudio@email.com";
+        urlPicture = "http://photo";
+        idRestaurantPicked = "123456987";
+    }
+
+    @Test
+    public void getCorrectInfoFromUser() throws Exception {
+        assertEquals(uid, user.getUid());
+    }
+}
