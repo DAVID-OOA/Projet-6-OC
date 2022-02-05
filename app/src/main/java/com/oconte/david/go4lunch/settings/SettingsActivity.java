a1b3399a5fa85be561929e310a2d43b90d582fae
Oconte David <david.oconte@orange.fr>
2022-02-05 14:06:41 +0100

    I add for settings.

--- app/src/main/java/com/oconte/david/go4lunch/settings/SettingsActivity.java
+++ app/src/main/java/com/oconte/david/go4lunch/settings/SettingsActivity.java
@@ -1,21 +1,32 @@
 package com.oconte.david.go4lunch.settings;
 
+import android.Manifest;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.oconte.david.go4lunch.injection.Injection;
 import com.oconte.david.go4lunch.R;
 import com.oconte.david.go4lunch.databinding.ActivitySettingsBinding;
-import com.oconte.david.go4lunch.restodetails.ViewModelFactory;
+import com.oconte.david.go4lunch.injection.Injection;
+import com.oconte.david.go4lunch.repositories.ViewModelFactory;
 
 import java.util.Objects;
+
+import butterknife.OnClick;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -24,6 +35,10 @@ public class SettingsActivity extends AppCompatActivity {
     private SettingsViewModel viewModel;
 
     //String username;
+    // DATA FOR PICTURE
+    private static final String PERMS = Manifest.permission.READ_EXTERNAL_STORAGE;
+    private static final int RC_IMAGE_PERMS = 100;
+    private static final int RC_CHOOSE_PHOTO = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +55,27 @@ public class SettingsActivity extends AppCompatActivity {
 
         this.updateInfoOfUser(username);
 
+        this.addPhotoUser();
+
     }
 
     public void configureViewDetailsRestaurantFactory(FirebaseAuth firebaseAuth, FirebaseFirestore firebaseFirestore) {
         ViewModelFactory viewModelFactory = Injection.provideViewModelFactory(firebaseAuth,firebaseFirestore);
         ViewModelProvider viewModelProvider = new ViewModelProvider(SettingsActivity.this, viewModelFactory);
         viewModel = viewModelProvider.get(SettingsViewModel.class);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.handleResponse(requestCode, resultCode, data);
     }
 
     @Override
@@ -72,7 +102,7 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     // FOR DELETE ACCOUNT
-    //@OnClick(R.id.profile_activity_button_delete)
+    @OnClick(R.id.delete_button)
     public void onClickDeleteButton() {
         new AlertDialog.Builder(this)
                 .setMessage(R.string.popup_message_confirmation_delete_account)
@@ -87,5 +117,61 @@ public class SettingsActivity extends AppCompatActivity {
                 .show();
     }
 
+    public void addPhotoUser() {
+        binding.photoUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImageFromPhone();
+            }
+        });
+    }
+
+    /*private ButtonActionListener getButtonActionListener(){
+        return view -> {
+            int id = view.getId();
+            switch (id){
+                case R.id.notification_switch:
+                    //viewModel.notificationStateChanged(((SwitchCompat) view).isChecked());
+                    break;
+                case R.id.update_button:
+                    //viewModel.updateUserInfo();
+                    break;
+                case R.id.delete_button:
+                    //viewModel.deleteUserFromDBRequest();
+                    break;
+                case R.id.photo_user:
+                    chooseImageFromPhone();
+                    break;
+            }
+
+        };
+    }*/
+
+    // --------------------
     // FOR ADD PHOTO
+    // --------------------
+
+    @AfterPermissionGranted(RC_IMAGE_PERMS)
+    private void chooseImageFromPhone() {
+        if(! EasyPermissions.hasPermissions(this, PERMS)){
+            EasyPermissions.requestPermissions(
+                    this, getString(R.string.need_permission_message), RC_IMAGE_PERMS, PERMS);
+            return;
+        }
+        Log.e("here", "ehre");
+        Intent photoIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(photoIntent, RC_CHOOSE_PHOTO);
+    }
+
+    private void handleResponse(int requestCode, int resultCode, Intent data){
+        if(requestCode == RC_CHOOSE_PHOTO){
+            if (resultCode == RESULT_OK){
+                Uri uriImage = data.getData();
+                if(uriImage != null) {
+                    viewModel.updateUserPhoto(uriImage.toString());
+                }
+            }
+        }
+
+    }
 }
--- app/src/main/java/com/oconte/david/go4lunch/settings/SettingsViewModel.java
+++ app/src/main/java/com/oconte/david/go4lunch/settings/SettingsViewModel.java
@@ -1,18 +1,124 @@
 package com.oconte.david.go4lunch.settings;
 
+import static com.oconte.david.go4lunch.util.RetryAction.UPDATE_PHOTO_DB;
+import static com.oconte.david.go4lunch.util.SuccessOrigin.UPDATE_PHOTO;
+import static com.oconte.david.go4lunch.util.TextUtil.isEmailCorrect;
+import static com.oconte.david.go4lunch.util.TextUtil.isTextLongEnough;
+
+import android.net.Uri;
+
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.oconte.david.go4lunch.workMates.UserRepository;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.events.Event;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.oconte.david.go4lunch.R;
+import com.oconte.david.go4lunch.models.User;
+import com.oconte.david.go4lunch.repositories.UserRepository;
+import com.oconte.david.go4lunch.util.SuccessOrigin;
+
+import java.util.Objects;
+import java.util.UUID;
 
 public class SettingsViewModel extends ViewModel {
 
+    // PUBLIC LIVE DATA
+    public MutableLiveData<String> urlPicture = new MutableLiveData<>();
+    public MutableLiveData<Boolean> isEmailError = new MutableLiveData<>();
+    public MutableLiveData<Integer> errorMessageEmail = new MutableLiveData<>();
+    public MutableLiveData<Boolean> isUsernameError = new MutableLiveData<>();
+    public MutableLiveData<Integer> errorMessageUsername = new MutableLiveData<>();
+
     private final UserRepository userRepository;
+
+    private FirebaseUser user;
+
+    private String newPhotoUrl;
 
     public SettingsViewModel(UserRepository userRepository) {
         this.userRepository = userRepository;
+        user = userRepository.getCurrentUser();
     }
 
     public void updateUserName(String username) {
         userRepository.updateUsername(username);
+    }
+
+    public void updateUserPhoto(String toString) {
+
+    }
+
+    // --------------------
+    // SET NEW PICTURE
+    // --------------------
+
+    private void uploadPhotoInFirebase(final String urlPhoto) {
+        String uuid = UUID.randomUUID().toString();
+        StorageReference imageRef = FirebaseStorage.getInstance().getReference(uuid);
+        imageRef.putFile(Uri.parse(urlPhoto))
+                .addOnSuccessListener(this::getUrlPhotoFromFirebase);
+                //.addOnFailureListener(this.onFailureListener(UPDATE_PHOTO_DB));
+
+    }
+
+    private void getUrlPhotoFromFirebase(UploadTask.TaskSnapshot taskSnapshot){
+        Objects.requireNonNull(Objects.requireNonNull(taskSnapshot.getMetadata()).getReference()).getDownloadUrl()
+                .addOnSuccessListener(uri -> {
+                    newPhotoUrl = uri.toString();
+                    userRepository.updateUrlPicture(newPhotoUrl, String.valueOf(userRepository.getCurrentUser()))
+                            .addOnSuccessListener(onSuccessListener(UPDATE_PHOTO));
+                            //.addOnFailureListener(this.onFailureListener(UPDATE_PHOTO_DB));
+                });//.addOnFailureListener(this.onFailureListener(UPDATE_PHOTO_DB));
+
+    }
+
+    // --------------------
+    // UTILS
+    // --------------------
+
+    private boolean isNewUserInfosCorrect(String email, String username){
+        if(!isEmailCorrect(email)){
+            isEmailError.setValue(true);
+            errorMessageEmail.setValue(R.string.incorrect_email);
+            return false;
+        }
+        if(!isTextLongEnough(username, 3)){
+            isUsernameError.setValue(true);
+            errorMessageUsername.setValue(R.string.incorrect_username);
+            return false;
+        }
+
+        return true;
+
+    }
+
+    private OnSuccessListener<Void> onSuccessListener(final SuccessOrigin origin){
+        return aVoid -> {
+            switch (origin){
+                case UPDATE_USER:
+                    //snackBarText.setValue(new Event<>(R.string.information_updated));
+                    //isLoading.setValue(false);
+                    //user.setEmail(newEmail);
+                    //user.setUsername(newUsername);
+                    break;
+                case DELETE_USER:
+                    //snackBarText.setValue(new Event<>(R.string.deleted_account_message));
+                    //deleteUser.setValue(new Event<>(new Object()));
+                    //isLoading.setValue(false);
+                    break;
+                case UPDATE_PHOTO:
+                    //snackBarText.setValue(new Event<>(R.string.photo_updated_message));
+                    //isLoading.setValue(false);
+                    urlPicture.setValue(newPhotoUrl);
+                    //user.setUrlPicture(newPhotoUrl);
+                    break;
+            }
+
+        };
+
     }
 }
