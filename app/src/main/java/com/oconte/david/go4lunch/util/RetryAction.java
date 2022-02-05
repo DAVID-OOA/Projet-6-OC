6f74f7f5754731a287762b9d3dad32a104b69e7d
Oconte David <david.oconte@orange.fr>
2022-02-05 14:00:36 +0100

    I add that for settings.

---
+++ app/src/main/java/com/oconte/david/go4lunch/util/RetryAction.java
@@ -0,0 +1,8 @@
+package com.oconte.david.go4lunch.util;
+
+public enum RetryAction {
+    UPDATE_PHOTO_DB,
+    UPDATE_INFO_USER_DB,
+    DELETE_USER_DB,
+    LOGOUT,
+}
---
+++ app/src/main/java/com/oconte/david/go4lunch/util/SuccessOrigin.java
@@ -0,0 +1,7 @@
+package com.oconte.david.go4lunch.util;
+
+public enum SuccessOrigin {
+    UPDATE_USER,
+    DELETE_USER,
+    UPDATE_PHOTO,
+}
