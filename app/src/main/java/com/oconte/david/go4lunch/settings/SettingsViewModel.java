70d0e9b3c71dc86bee2f73b69749d20dd323894f
Oconte David <david.oconte@orange.fr>
2022-04-07 15:31:10 +0200

    I clean the code.

--- app/src/main/java/com/oconte/david/go4lunch/settings/SettingsViewModel.java
+++ app/src/main/java/com/oconte/david/go4lunch/settings/SettingsViewModel.java
@@ -1,36 +1,19 @@
 package com.oconte.david.go4lunch.settings;
 
-import static com.oconte.david.go4lunch.util.RetryAction.UPDATE_PHOTO_DB;
-import static com.oconte.david.go4lunch.util.SuccessOrigin.UPDATE_PHOTO;
-import static com.oconte.david.go4lunch.util.TextUtil.isEmailCorrect;
-import static com.oconte.david.go4lunch.util.TextUtil.isTextLongEnough;
-
 import android.net.Uri;
 
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-import com.oconte.david.go4lunch.R;
 import com.oconte.david.go4lunch.repositories.UserRepository;
-import com.oconte.david.go4lunch.util.SuccessOrigin;
 
 import java.util.Objects;
 import java.util.UUID;
 
 public class SettingsViewModel extends ViewModel {
-
-    // PUBLIC LIVE DATA
-    public MutableLiveData<String> urlPicture = new MutableLiveData<>();
-    public MutableLiveData<Boolean> isEmailError = new MutableLiveData<>();
-    public MutableLiveData<Integer> errorMessageEmail = new MutableLiveData<>();
-    public MutableLiveData<Boolean> isUsernameError = new MutableLiveData<>();
-    public MutableLiveData<Integer> errorMessageUsername = new MutableLiveData<>();
 
     private final UserRepository userRepository;
 
@@ -43,14 +26,17 @@ public class SettingsViewModel extends ViewModel {
         user = userRepository.getCurrentUser();
     }
 
+    // For update User Name
     public void updateUserName(String username) {
         userRepository.updateUsername(username);
     }
 
+    // For update Email
     public void updateEmail(String email) {
         userRepository.updateEmail(email);
     }
 
+    // For update a new user photo
     public void updatePhotoUser(String newPhotoUrl) {
         uploadPhotoInFirebase(newPhotoUrl);
     }
@@ -71,7 +57,6 @@ public class SettingsViewModel extends ViewModel {
                 });
 
     }
-
 
     // Delete User
     public void deleteUser(String uid) {
