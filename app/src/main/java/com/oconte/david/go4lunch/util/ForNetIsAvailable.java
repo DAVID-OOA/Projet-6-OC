f798a1d2a6ac04749d33a370b4212cccee65ddd1
Oconte David <david.oconte@orange.fr>
2022-04-22 15:27:15 +0200

    I clean the code.

--- app/src/main/java/com/oconte/david/go4lunch/util/ForNetIsAvailable.java
+++ app/src/main/java/com/oconte/david/go4lunch/util/ForNetIsAvailable.java
@@ -3,11 +3,6 @@ package com.oconte.david.go4lunch.util;
 import android.content.Context;
 import android.net.ConnectivityManager;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-
 public class ForNetIsAvailable {
 
     public static boolean isNetworkConnected(Context context) {
